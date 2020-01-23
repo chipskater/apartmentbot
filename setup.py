bc0534a1cbac8ecad647d3edc2c6e1c7d3462a36
Nico Matzutt <nico.matzutt@gmail.com>
2020-01-23 19:39:52 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='apartmentbot',
+      version='0.1',
+      description='Telegram Bot for finding apartment listings',
+      url='http://github.com/chipskater/apartmentbot',
+      author='Nico Matzutt',
+      author_email='nico.matzutt@gmail.com',
+      license='MIT',
+      packages=['apartmentbot'],
+      zip_safe=False)
